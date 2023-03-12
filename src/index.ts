03afbcf4871fad4a550fdc9b0326f6c97ba551d0
Agustín Charry <hacharry@bancolombia.com.co>
2023-03-11 21:01:55 -0500

    Now NoteValueManager is a static class

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
 import { MelodyMaker } from './MelodyMaker';
+import { NoteValueManager } from './NoteValue/NoteValueManager';
 
-const result = MelodyMaker.generate(192, ['2/4', '1/8', '4/4', '1/4'], [10, 40, 30, 20])
+const wantedValue: number = NoteValueManager.getNoteValueById('4/4').getSymbolicDuration();
+const result = MelodyMaker.generate(wantedValue, ['2/4', '1/8', '4/4', '1/4'], [10, 40, 30, 20])
 
 console.log(result.toString())
